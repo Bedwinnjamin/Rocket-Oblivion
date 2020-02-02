abf29097500350a57517011bb7fc924f1125ee7b
Josiah Lansford <39782615+josassy@users.noreply.github.com>
2020-02-02 14:15:44 -0500

    added cool looking bars

--- entities/UI/Bar.gd
+++ entities/UI/Bar.gd
@@ -1,6 +1,11 @@
 extends ProgressBar
 
 export (int) var bar_id
+export (String) var bar_name
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = bar_name
 
 func _update_resource(id, value):
 	if id == bar_id:
