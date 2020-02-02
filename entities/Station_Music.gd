e09327d9b81a9e5a903aa7c6d68f7c80e1c97aa7
Luke Bullis <lukebullis@cedarforest.cedarville.edu>
2020-02-02 15:57:46 -0500

    Missions work, will need to update balance

--- entities/Station_Music.gd
+++ entities/Station_Music.gd
@@ -22,7 +22,7 @@ func _switch_music(crew_id):
 	track_position = self.get_playback_position()
 	self.set_stream(songs[crew_id])
 	self.play(track_position)
-	tween_out.interpolate_property(self, "volume_db", -80, -20, transition_duration, transition_type, Tween.EASE_IN, 0)
+	tween_out.interpolate_property(self, "volume_db", -80, -5, transition_duration, transition_type, Tween.EASE_IN, 0)
 	tween_out.start()
 
 func _turn_off():
