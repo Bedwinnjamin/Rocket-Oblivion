5416951768cb5597ecc4b8fdeb9c04231a8dd0b3
Josiah Lansford <39782615+josassy@users.noreply.github.com>
2020-02-02 15:55:42 -0500

    made console look nicer

--- entities/UI/Console.gd
+++ entities/UI/Console.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-func _ready():
-	$RichTextLabel.set_scroll_follow(true)
-
 func _show_message(message):
-	$RichTextLabel.text += message + '\n'
+	$Panel/RichTextLabel.text += message + '\n'
 	
 func _clear_message():
-	$RichTextLabel.text = ""
+	$Panel/RichTextLabel.text = ""
 
